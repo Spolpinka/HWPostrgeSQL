9a8b03683bd17ffbaa3548f85adaafe6c3c6e8fd
Spolpinka <a421243266@gmail.com>
2023-04-27 07:01:05 +0300

    добавлена функция Update

--- src/main/java/pro/sky/hwpostrgesql/repository/EmloyeeDao.java
+++ src/main/java/pro/sky/hwpostrgesql/repository/EmloyeeDao.java
@@ -1,2 +1,18 @@
-package pro.sky.hwpostrgesql.repository;public class EmloyeeDao {
+package pro.sky.hwpostrgesql.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmloyeeDao {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void updateData() {
+        String sql = "UPDATE employee SET first_name = ?, last_name = ? WHERE условие";
+        jdbcTemplate.update(sql, "новое_значение1", "новое_значение2");
+    }
+
 }
